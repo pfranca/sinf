c6a546d36a0449659cec594fcfd36e3e4658784b
pfranca <pedro.franca.1994@gmail.com>
2018-11-23 17:48:19 +0000

    initial commit. base structure

---
+++ app.js
@@ -0,0 +1,29 @@
+var createError = require('http-errors');
+var express = require('express');
+var exphbs = require('express-handlebars')
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+
+// view engine setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+module.exports = app;
