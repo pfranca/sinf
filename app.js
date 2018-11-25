b1e8eaef9383964219c4f5aa6d411dcf507655fe
FilipePintoReis <filipepintodosreis@gmail.com>
2018-11-25 20:33:35 +0000

    added json for request package

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var checkoutRouter = require('./routes/checkout');
 var app = express();
 
 
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/checkout',checkoutRouter);
 
 
 module.exports = app;
