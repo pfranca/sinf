d47295b613baeeda066e82061ea8a83f2d56a702
Simão L. Lúcio <up201303845@fe.up.pt>
2018-11-29 19:00:51 +0000

    token fixed

--- app.js
+++ app.js
@@ -21,9 +21,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //URL base da API
 global.url = "http://localhost:2018/WebApi/";
+global.token = "";
+
 //A função abaixo corre sempre que é feito um pedido a qualquer rota de forma a definir um token
 //a ser usado a nivel global
 app.use(async (req, res, next) =>{
+    if(token == ""){
     let params ={
         username: 'FEUP',
         password: 'qualquer1',
@@ -32,15 +35,16 @@ app.use(async (req, res, next) =>{
         grant_type: 'password',
         line: 'professional'
     };
-    await request.post({url: url+"/token", form:params}, (error, response, body) => {
+    await request.post({url: url+"token", form:params}, (error, response, body) => {
         if (error) {
             console.error(error);
             return;
         } else {
             jsonArray = JSON.parse(body);
-            global.token = jsonArray["access_token"];
+            token = jsonArray["access_token"];
         }
     });
+    }
     next();
 });
 
